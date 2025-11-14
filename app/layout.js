4731b748f883a0db5ae235ddf102dbfdb1998b98
Aidhandy <aidhandy.inc@gmail.com>
2025-11-14 17:38:53 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 "use client";
 
-import "../globals.css";        // <-- THIS WAS YOUR BUG
-import { useState } from "react";
-import Link from "next/link";
+import "../globals.css";
 import Navbar from "./navbar.jsx";
 
 export const metadata = {
